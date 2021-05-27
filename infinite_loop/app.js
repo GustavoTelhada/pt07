f87eaa6f204b74f6f468ba37592ca23cf5da9cfa
GustavoTelhada <71329527+GusTelhada@users.noreply.github.com>
2021-05-27 18:22:56 +0100

    update

---
+++ infinite_loop/app.js
@@ -0,0 +1,27 @@
+var text
+function BrowserCrash(){
+    var n = prompt("Quantas pizzas queres?")
+    while (n != null) {
+
+        var i = 1
+
+        if (i > n) {
+            alert("Não gostas de pizza? :(");
+            break
+            }
+
+        else if (i <= n){
+            console.log("depois else if");
+            text = ""
+            while (i <= n) {
+                console.log("depois while");
+            text = text + "pizza, "
+
+            i = i + 1
+            }
+            console.log("fim");
+            document.getElementById("p-pizzas").innerHTML = text;
+            break
+            }
+    }
+}
